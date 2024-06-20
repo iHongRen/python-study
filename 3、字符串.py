54485dcc3bd376bc3703ccc08ecbbd5ca8128121
chenxianyin <iamhongren@gmail.com>
2024-06-20 15:40:45 +0800

    add: 3、字符串.py

---
+++ 3、字符串.py
@@ -0,0 +1,124 @@
+# 在 Python 中，字符串是以单引号或双引号或三引号引起来的。
+s0 = 'hello world'
+s1 = "Hello!"
+s2 = '''
+hello
+world
+aaa
+bbb
+'''
+
+# 原始字符串，以 r 开头，不会发生转义
+s3 = r'hello \n world'
+print(s3)
+
+# 直接使用 Unicode 编码表示字符
+s4 = '\u9000'
+print(s4)
+
+# 字符串拼接
+s5 = s1 + s2
+s6 = '11' + ' ' + '22'
+print(s5)
+print(s6)
+
+s5 += s5
+s6 *= 2
+print(s5)
+print(s6)
+
+s7 = 'a' * 10
+print(s7)
+
+# 比较内存地址
+print(s1 is s2)
+
+# in 和 not in
+print('hello' in s2)
+print('hello' not in s2)
+
+# 字符串长度
+length = len(s2)
+print(length)
+
+# 字符串切片
+s8 = '012345678'
+print(s8[0])   # 第 0 个
+print(s8[0:5]) # 从 0 开始，到 5 结束，不包括 5
+print(s8[6:])  # 从 6 开始，到结尾
+print(s8[:6])  # 从 0 开始，到 6 结束，不包括 6
+print(s8[::2]) # 从 0 开始，到 结尾，步长为 2
+print(s8[::-1]) # 从结尾开始，到 0 结束，步长为1
+print(s8[::-2]) # 从结尾开始，到 0 结束，步长为2
+
+# 字符串遍历
+for c in s1:
+    print(c)
+
+for i in range(len(s1)):
+    print(s1[i])
+
+# 字符串一些方法
+s9 = 'hello world'
+print(s9.upper()) # 全部大写
+print(s9.lower()) # 全部小写
+print(s9.capitalize()) # 首字母大写
+print(s9.title()) # 首字母大写
+print(s9.swapcase()) # 大小写翻转
+
+print(s9.count('l')) # 统计 'l' 出现的次数
+print(s9.find('l')) # 查找 'l' 的位置，找不到返回 -1
+print(s9.rfind('l')) # 反向查找 'l' 的位置，找不到返回 -1
+
+print(s9.index('l')) # 查找 'l' 的位置，找不到会报错
+print(s9.rindex('l')) # 反向查找 'l' 的位置，找不到会报错
+
+print(s9.center(20)) # 居中，长度为 20，用空格填充
+print(s9.center(20, '*')) # 居中，长度为 20，用 '*' 填充， ****hello world*****
+print(s9.ljust(20)) # 左对齐，长度为 20，用空格填充
+print(s9.ljust(20, '*')) # 左对齐，长度为 20，用 '*' 填充，hello world*********
+print(s9.rjust(20)) # 右对齐，长度为 20，用空格填充
+print(s9.rjust(20, '*')) # 右对齐，长度为 20，用 '*' 填充，*********hello world
+print(s9.zfill(20)) # 左对齐，长度为 20，用 '0' 填充，000000000hello world
+
+print(s9.startswith('He')) # 判断是否以 'He' 开头
+print(s9.endswith('d')) # 判断是否以 'd' 结尾
+
+s10 = '123456'
+print(s10.isalpha()) # 判断是否全为字母
+print(s10.isdigit()) # 判断是否全为数字
+print(s10.isalnum()) # 判断是否全为字母或数字
+print(s10.isdecimal()) # 判断是否全为十进制数字
+print(s10.isspace()) # 判断是否全为空白字符
+print(s10.isnumeric()) # 判断是否全为数字
+print(s10.isascii()) # 判断是否全为 ASCII 字符
+
+
+# 格式化
+a = 10
+b = 20
+print('%d + %d = %d' % (a,b, a+b))
+print('{} * {} = {}'.format(a,b, a*b))
+print(f'{a} / {b} = {a/b}')
+
+# 去掉空格
+s11 = '  hello  '
+print(s11.strip()) # 去掉两边的空格
+print(s11.lstrip()) # 去掉左边的空格
+print(s11.rstrip()) # 去掉右边的空格
+
+# 替换
+print(s11.replace('hello', 'world'))
+print(s11.replace('hello', 'world', 1)) # 第三个参数指定替换的次数
+
+# 分割/合并
+print(s11.split(' '))
+print(' '.join(['hello', 'world', 'aaa', 'bbb']))
+
+# 编码和解码
+# 编码和解码的方式不一致，会导致乱码
+s12 = '中'
+print(s12.encode('gbk'))
+s13 = s12.encode('utf-8')
+print(s13)
+print(s13.decode('utf-8'))
