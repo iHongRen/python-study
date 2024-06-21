38e6a0275b9131884dd095d1f487be177b6bd89c
chenxianyin <iamhongren@gmail.com>
2024-06-21 11:19:21 +0800

    fix: 增加结果输出

--- 1、基础数据类型.py
+++ 1、基础数据类型.py
@@ -4,9 +4,9 @@
 #   复数(complex): 1j, 1+2j
 
 # 可以使用 type() 函数查看数据类型
-print(type(-2))
-print(type(3.14))
-print(type(1j))
+print(type(-2)) # 输出:<class 'int'>
+print(type(3.14)) # 输出:<class 'float'>
+print(type(1j)) # 输出:<class 'complex'>
 
 # 定义变量，初始化赋值，不需要写类型
 a = 1
@@ -14,13 +14,13 @@ b = 3.14
 c = 1j
 
 # 布尔类型(bool)：True, False
-print(type(True))
-print(a==c)
+print(type(True)) # 输出: <class 'bool'>
+print(a==c) # 输出: False
 
 
 # 字符串类型(str)：
 #   单引号、双引号、三引号
-print(type('hello'))
+print(type('hello')) # 输出: <class 'str'>
 h = "hello world"
 print(h)
 
@@ -33,89 +33,90 @@ print(myWord)
 
 # 列表类型(list)：使用方括号 [] 表示
 myList = [1, 2, 3]
-print(type(myList))
+print(type(myList)) # 输出: <class 'list'>
 print(['a', 'b', 'c'])
 
 # 元组类型(tuple)：使用圆括号 () 表示
 myTuple = (1, 2, 3)
-print(type(myTuple))
+print(type(myTuple)) # 输出: <class 'tuple'>
 print(('404', 'not found'))
 
 # 字典类型(dict)：使用大括号 {} 表示, 键值对形式
 myDict = {'name': 'hongren', 'age': 18}
-print(type(myDict))
+print(type(myDict)) # 输出: <class 'dict'>
 print({'name': '张三', 'age': 18, 'gender': '男'})
 
 # 集合类型(set)：也使用大括号 {} 表示
 mySet = {1, 2, 3}
-print(type(mySet))
+print(type(mySet)) # 输出: <class 'set'>
 print({'apple', 'banana', 'cherry'})
 
 # 空值类型(NoneType)：None
-print(type(None))
+print(type(None)) # 输出: <class 'NoneType'>
 
 
 # 字节类型（bytes）：用于存储二进制数据
 a = b"hello"
-print(type(a))
+print(type(a)) # 输出: <class 'bytes'>
 
 """ 
  运算符
 """
-print(100 + 200) # 加法
-print(100 - 200) # 减法
-print(100 * 200) # 乘法
-print(100 / 200) # 除法 结果为浮点数
-print(100 // 200) # 取整
-print(100 % 200) # 取余
-print(2 ** 10) # 求幂 2的10次方 
+print(100 + 200) # 加法, 输出: 300
+print(100 - 200) # 减法, 输出: -100
+print(100 * 200) # 乘法, 输出: 20000
+print(100 / 200) # 除法 结果为浮点数, 输出: 0.5
+print(100 // 200) # 取整, 输出: 0
+print(100 % 200) # 取余, 输出: 100
+print(2 ** 10) # 求幂 2的10次方, 输出: 1024
 
 a = 10
 b = 5.5
 
-a += b   # 等价于 a = a + b
-print(a) # 15.5 类似的 -=, *=, /=, //=, **=
+a += b   # 等价于 a = a + b, 类似的还有 -=, *=, /=, //=, **=
+print(a) # 输出: 15.5 
 
 # 比较
-print(a==b)
-print(a!=b)
-print(a>b)
-print(a<b)
-print(a>=b)
-print(a<=b)
+print(a==b) # 输出: False
+print(a!=b) # 输出: True
+print(a>b) # 输出: True
+print(a<b) # 输出: False
+print(a>=b) # 输出: True
+print(a<=b) # 输出: False
 
 # 逻辑运算: and, or, not
-print(a and b) # 逻辑与
-print(a or b) # 逻辑或
-print( not a) # 逻辑非
+print(a>10 and b>10) # 逻辑与, 输出: False
+print(a>10 or b>10) # 逻辑或, 输出: True
+print(not a>10) # 逻辑非, 输出: False
 
 # 位运算: &, |, ^, ~, <<, >>
-a = 10
-b = 20
-print(a & b) # 位与
-print(a | b) # 位或
-print(a ^ b) # 位异或
-print(~a) # 位非
-print(a << 2) # 左移
-print(a >> 2) # 右移
+a = 1
+b = 0
+print(a & b) # 位与, 输出: 0
+print(a | b) # 位或, 输出: 1
+print(a ^ b) # 位异或, 输出: 1
+print(~a) # 位非, 等价于 -(a + 1), 输出: -2
+print(a << 3) # 左移, 等价于a * 2的3次方, 输出: 8
+print(a >> 3) # 右移, 等价于a / 2的3次方, 输出: 0
 
 # 成员运算: in, not in
-print(1 in [1, 2, 3])
-print(1 not in [1, 2, 3])
-print('a' in 'hello')
-print('a' not in 'hello')
+print(1 in [1, 2, 3]) # 输出: True
+print(1 not in [1, 2, 3]) # 输出: False
+print('a' in 'hello') # 输出: False
+print('a' not in 'hello') # 输出: True
 
 # 身份运算: is, is not
+# is 用于判断两个变量引用对象是否为同一个， == 用于判断引用变量的值是否相等。
+
 a = [1, 2, 3]
 b = [1, 2, 3]
 
-# is 用于判断两个变量引用对象是否为同一个， == 用于判断引用变量的值是否相等。
-print(a is b)
-print(a == b)
+print(a is b) # 输出: False
+print(a == b) # 输出: True
 
 #  id() 函数用于获取对象内存地址
-print(id(a))
-print(id(b))
+print(id(a)) # 输出: 4506456128 (每次打印的值都不一样)
+print(id(b)) # 输出: 4506758656
 
 
 
@@ -127,30 +128,30 @@ b = 6.18
 c = '3.14'
 d = True
 
-print(str(a))
-print(int(b)) # 转为整数,转不成功会报异常
-print(float(c))
-print(int(d))
-print(bool(a))
-print(str([1,2,3])) # 将列表转为字符串
+print(str(a)) # 输出: '1'
+print(int(b)) # 输出: 6,转不成功会报异常
+print(float(c)) # 输出: 3.14
+print(int(d)) # 输出: 1
+print(bool(a)) # 输出: True
+print(str([1,2,3])) # 输出: '[1,2,3]'
 
 print(chr(65)) # 将65转为转为ascii 'A',
 print(ord("A")) # 将'A'转为对应的整数 65
 
 # 一些数学函数
-print(max(1, 2, 3)) # 求最大值
-print(min(1, 2, 3)) # 求最小值
-print(sum([1, 2, 3])) # 求和
-print(abs(-10)) # 取绝对值
-print(pow(2, 3)) # 2的3次方
-print(round(3.1415926)) # 四舍五入
-print(round(3.1415926, 2)) # 保留两位小数
+print(max(1, 2, 3)) # 求最大值, 输出: 3
+print(min(1, 2, 3)) # 求最小值, 输出: 1
+print(sum([1, 2, 3])) # 求和, 输出: 6
+print(abs(-10)) # 取绝对值, 输出: 10
+print(pow(2, 3)) # 2的3次方, 输出: 8
+print(round(3.1415926)) # 四舍五入, 输出: 3
+print(round(3.1415926, 2)) # 保留两位小数, 输出: 3.14
 
 import math
-print(math.fabs(-10.12)) # 浮点数取绝对值
-print(math.ceil(3.1415926)) # 向上取整
-print(math.floor(3.1415926)) # 向下取整
-print(math.sqrt(9)) # 平方根
+print(math.fabs(-10.12)) # 浮点数取绝对值, 输出: 10.12
+print(math.ceil(3.1415926)) # 向上取整, 输出: 4
+print(math.floor(3.1415926)) # 向下取整, 输出: 3
+print(math.sqrt(9)) # 平方根, 输出: 3.0
 
 # 随机数
 import random
@@ -164,7 +165,7 @@ print(random.sample('abcdefg', 3)) # 从字符串中随机选择三个字母
 # 字符串转字节
 s = "apple"
 a = bytes(s, encoding="utf-8")
-print(a)
+print(a) # 输出: b'apple'
 
 for i in a:
     print(i)
@@ -173,9 +174,9 @@ for i in a:
 """
   进制转换
 """
-print(bin(10)) # 将10转为二进制
-print(oct(10)) # 八进制
-print(hex(10)) # 十六进制
+print(bin(10)) # 将10转为二进制, 输出: 0b1010
+print(oct(10)) # 八进制, 输出: 0o12
+print(hex(10)) # 十六进制, 输出: 0xa
 
 a = bin(10)
-print(int(a, 2)) # 二进制转十进制
+print(int(a, 2)) # 二进制转十进制, 输出: 10
--- 3、字符串.py
+++ 3、字符串.py
@@ -1,6 +1,6 @@
 # 在 Python 中，字符串是以单引号或双引号或三引号引起来的。
-s0 = 'hello world'
-s1 = "Hello!"
+s0 = 'hello'
+s1 = "world"
 s2 = '''
 hello
 world
@@ -10,46 +10,42 @@ bbb
 
 # 原始字符串，以 r 开头，不会发生转义
 s3 = r'hello \n world'
-print(s3)
-
-# 直接使用 Unicode 编码表示字符
-s4 = '\u9000'
-print(s4)
+print(s3) # 输出：hello \n world
 
 # 字符串拼接
 s5 = s1 + s2
 s6 = '11' + ' ' + '22'
-print(s5)
-print(s6)
+print(s5) # 输出: helloworld
+print(s6) # 输出: 11 22
 
-s5 += s5
-s6 *= 2
-print(s5)
-print(s6)
+s5 += s5 # 字符串拼接
+s6 *= 2 # 字符串重复
+print(s5) # 输出: helloworldhelloworld
+print(s6) # 输出: 11 2211 22
 
 s7 = 'a' * 10
-print(s7)
+print(s7) # 输出: aaaaaaaaaa
 
 # 比较内存地址
-print(s1 is s2)
+print(s1 is s2) # 输出: False
 
 # in 和 not in
-print('hello' in s2)
-print('hello' not in s2)
+print('hello' in s2) # 输出: True
+print('hello' not in s2) # 输出: False
 
 # 字符串长度
-length = len(s2)
-print(length)
+length = len(s2) 
+print(length) # 输出: 21
 
 # 字符串切片
 s8 = '012345678'
-print(s8[0])   # 第 0 个
-print(s8[0:5]) # 从 0 开始，到 5 结束，不包括 5
-print(s8[6:])  # 从 6 开始，到结尾
-print(s8[:6])  # 从 0 开始，到 6 结束，不包括 6
-print(s8[::2]) # 从 0 开始，到 结尾，步长为 2
-print(s8[::-1]) # 从结尾开始，到 0 结束，步长为1
-print(s8[::-2]) # 从结尾开始，到 0 结束，步长为2
+print(s8[0])   # 第 0 个， 输出: 0
+print(s8[0:5]) # 从 0 开始，到 5 结束，不包括 5, 输出: 01234
+print(s8[6:])  # 从 6 开始，到结尾, 输出: 678
+print(s8[:6])  # 从 0 开始，到 6 结束，不包括 6, 输出: 012345
+print(s8[::2]) # 从 0 开始，到 结尾，步长为 2, 输出: 02468
+print(s8[::-1]) # 从结尾开始，到 0 结束，步长为1, 输出: 876543210
+print(s8[::-2]) # 从结尾开始，到 0 结束，步长为2, 输出: 86420
 
 # 字符串遍历
 for c in s1:
@@ -60,65 +56,73 @@ for i in range(len(s1)):
 
 # 字符串一些方法
 s9 = 'hello world'
-print(s9.upper()) # 全部大写
-print(s9.lower()) # 全部小写
-print(s9.capitalize()) # 首字母大写
-print(s9.title()) # 首字母大写
-print(s9.swapcase()) # 大小写翻转
+print(s9.upper()) # 全部大写, 输出: HELLO WORLD
+print(s9.lower()) # 全部小写, 输出: hello world
+print(s9.capitalize()) # 首字母大写, 输出: Hello world
+print(s9.title()) # 首字母大写, 输出: Hello World
+print(s9.swapcase()) # 大小写翻转, 输出: HELLO wORLD
 
-print(s9.count('l')) # 统计 'l' 出现的次数
-print(s9.find('l')) # 查找 'l' 的位置，找不到返回 -1
-print(s9.rfind('l')) # 反向查找 'l' 的位置，找不到返回 -1
+print(s9.count('l')) # 统计 'l' 出现的次数, 输出: 3
+print(s9.find('l')) # 查找 'l' 的位置，找不到返回 -1, 输出: 2
+print(s9.rfind('l')) # 反向查找 'l' 的位置，找不到返回 -1, 输出: 9
 
-print(s9.index('l')) # 查找 'l' 的位置，找不到会报错
-print(s9.rindex('l')) # 反向查找 'l' 的位置，找不到会报错
+print(s9.index('l')) # 查找 'l' 的位置，找不到会报错, 输出: 2
+print(s9.rindex('l')) # 反向查找 'l' 的位置，找不到会报错, 输出: 9
 
-print(s9.center(20)) # 居中，长度为 20，用空格填充
-print(s9.center(20, '*')) # 居中，长度为 20，用 '*' 填充， ****hello world*****
-print(s9.ljust(20)) # 左对齐，长度为 20，用空格填充
-print(s9.ljust(20, '*')) # 左对齐，长度为 20，用 '*' 填充，hello world*********
-print(s9.rjust(20)) # 右对齐，长度为 20，用空格填充
-print(s9.rjust(20, '*')) # 右对齐，长度为 20，用 '*' 填充，*********hello world
-print(s9.zfill(20)) # 左对齐，长度为 20，用 '0' 填充，000000000hello world
+print(s9.center(20)) # 居中，长度为 20，用空格填充, 输出:     hello world
+print(s9.center(20, '*')) # 居中，长度为 20，用 '*' 填充，输出: ****hello world*****
+print(s9.ljust(20)) # 左对齐，长度为 20，用空格填充, 输出: hello world     
+print(s9.ljust(20, '*')) # 左对齐，长度为 20，用 '*' 填充，输出: hello world*********
+print(s9.rjust(20)) # 右对齐，长度为 20，用空格填充, 输出:          hello world
+print(s9.rjust(20, '*')) # 右对齐，长度为 20，用 '*' 填充, 输出: *********hello world
+print(s9.zfill(20)) # 左对齐，长度为 20，用 '0' 填充，输出: 000000000hello world
 
-print(s9.startswith('He')) # 判断是否以 'He' 开头
-print(s9.endswith('d')) # 判断是否以 'd' 结尾
+print(s9.startswith('He')) # 判断是否以 'He' 开头, 输出: False
+print(s9.startswith('hel')) # 判断是否以 'hel' 开头, 输出: True
+print(s9.endswith('d')) # 判断是否以 'd' 结尾, 输出: True
 
 s10 = '123456'
-print(s10.isalpha()) # 判断是否全为字母
-print(s10.isdigit()) # 判断是否全为数字
-print(s10.isalnum()) # 判断是否全为字母或数字
-print(s10.isdecimal()) # 判断是否全为十进制数字
-print(s10.isspace()) # 判断是否全为空白字符
-print(s10.isnumeric()) # 判断是否全为数字
-print(s10.isascii()) # 判断是否全为 ASCII 字符
+print(s10.isalpha()) # 判断是否全为字母, 输出: False
+print(s10.isdigit()) # 判断是否全为数字, 输出: True
+print(s10.isalnum()) # 判断是否全为字母或数字, 输出: True
+print(s10.isdecimal()) # 判断是否全为十进制数字, 输出: True
+print(s10.isspace()) # 判断是否全为空白字符, 输出: False
+print(s10.isnumeric()) # 判断是否全为数字, 输出: True
+print(s10.isascii()) # 判断是否全为 ASCII 字符, 输出: True
 
 
 # 格式化
 a = 10
 b = 20
-print('%d + %d = %d' % (a,b, a+b))
-print('{} * {} = {}'.format(a,b, a*b))
-print(f'{a} / {b} = {a/b}')
+print('%d + %d = %d' % (a,b, a+b)) # 输出: 10 + 20 = 30
+print('{} * {} = {}'.format(a,b, a*b)) # 输出: 10 * 20 = 200
+print(f'{a} / {b} = {a/b}') # 输出: 10 / 20 = 0.5
 
 # 去掉空格
 s11 = '  hello  '
-print(s11.strip()) # 去掉两边的空格
-print(s11.lstrip()) # 去掉左边的空格
-print(s11.rstrip()) # 去掉右边的空格
+print(s11.strip()) # 去掉两边的空格, 输出: hello
+print(s11.lstrip()) # 去掉左边的空格, 输出: hello   
+print(s11.rstrip()) # 去掉右边的空格, 输出:  hello
 
 # 替换
-print(s11.replace('hello', 'world'))
-print(s11.replace('hello', 'world', 1)) # 第三个参数指定替换的次数
+print(s11.replace('hello', 'world')) # 全部替换, 输出:   world
+print(s11.replace('hello', 'world', 1)) # 第三个参数指定替换的次数, 输出:  world
 
 # 分割/合并
-print(s11.split(' '))
-print(' '.join(['hello', 'world', 'aaa', 'bbb']))
+print(s11.split(' ')) # 按空格分割, 输出: ['', 'hello', '', '']
+print(' '.join(['hello', 'world', 'aaa', 'bbb'])) # 按空格合并, 输出: hello world aaa bbb
 
 # 编码和解码
 # 编码和解码的方式不一致，会导致乱码
 s12 = '中'
-print(s12.encode('gbk'))
-s13 = s12.encode('utf-8')
-print(s13)
-print(s13.decode('utf-8'))
+print(s12.encode('gbk')) # 输出: b'\xd6\xd0'
+s13 = s12.encode('utf-8') 
+print(s13) # 输出: b'\xe4\xb8\xad'
+print(s13.decode('utf-8')) # 输出: 中
+
+print(ord(s12)) # 打印码点 输出：20013
+print(chr(20013)) # 码点对应的字符 输出：中
+
+print(hex(ord(s12))) # 打印码点的十六进制 输出：0x4e2d
+print(chr(0x4e2d)) # 十六进制对应的字符 输出：中
+print('\u4e2d') # 直接打印 Unicode 字符 输出：中
